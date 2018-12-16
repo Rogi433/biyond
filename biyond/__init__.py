65878e8fc4dd7dbc193a6f398976ce39eed15d13
Igor Vieira <igor.vieira433@gmail.com>
2018-12-16 04:39:17 -0200

    adding enigmas

--- biyond/__init__.py
+++ biyond/__init__.py
@@ -1,12 +1,35 @@
-from flask import Flask, jsonify, request, abort, render_template
+from flask import Flask, jsonify, request, abort, render_template, redirect
 from werkzeug.exceptions import HTTPException
+from flask_mail import Mail, Message
 
 app = Flask(__name__, static_folder='../static', static_url_path='')
 
+app.config.update(dict(
+    MAIL_SERVER = 'smtp.googlemail.com',
+    MAIL_PORT = 587,
+    MAIL_USE_TLS = 1,
+    #MAIL_PORT=465,
+    #MAIL_USE_TLS=False,
+    #MAIL_USE_SSL=True,
+    MAIL_USERNAME = 'igor.vieira.fr',
+    MAIL_PASSWORD = 'igor4545'
+))
+
+mail = Mail(app)
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+
+@app.route('/process_email', methods=['POST'])
+def process_email():
+    rules = request.url_rule
+    msg = Message('Test', sender='igor.vieira.fr@gmail.com', recipients=['igor.vieira.fr@email.com'])
+    msg.body = 'This is a test email' #Customize based on user input
+    mail.send(msg)
+
+    return 'done'
 
 
 @app.route('/users', methods=['GET', 'POST'])
@@ -16,12 +39,7 @@ def get_or_post_users():
 
 @app.route('/users/<string:id>', methods=['DELETE'])
 def delete_user(id):
-    user = user_controller.delete(id)
-
-    if user:
-        return user.to_json(), 200
-    else:
-        abort(400)
+    return
 
 
 def run():
